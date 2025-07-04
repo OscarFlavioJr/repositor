d2c7a95ba5e1e147f366fca5810c0d4793a1c2f1
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2025-07-04 12:06:08 -0300

    First commit

---
+++ sub.rb
@@ -0,0 +1,20 @@
+loop do
+  puts "Cole o texto abaixo, e pressione Enter para substituir as vírgulas por traços:"
+  texto = gets.chomp
+  novo_texto = texto.gsub(',', ' -')
+  
+  puts "\n"
+
+  puts "Texto pronto para cópia logo abaixo:\n"
+  puts novo_texto
+
+  resposta = nil
+  loop do
+    puts "\nVocê gostaria de continuar usando o programa? \nS - sim\nN - não"
+    resposta = gets.chomp.downcase
+    break if ["s", "n"].include?(resposta)
+    puts "Por favor, digite uma resposta válida (S ou N)."
+  end
+
+  break if resposta == "n"
+end
